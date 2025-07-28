e78293a07c774c3f4f9257aa874dec80cb32ef51
FlameNotLame <93367144+FlameNotLame@users.noreply.github.com>
2025-07-29 00:39:50 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
-
 const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('GIF Generator backend is running!');
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
